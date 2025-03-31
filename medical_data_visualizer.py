30f13180d543fd7eacc35394f1933cbdbcf48bfc
JINM0RI <srivathsan.r2003@gmail.com>
2025-03-31 09:16:15 +0000

    completed

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = df=pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['height']=df['height']/100
+df['BMI']=df['weight']/(df['height']**2)
+df['overweight'] = (df['BMI']>25).astype(int)
+df.drop(columns=['BMI'],inplace=True)
 
 # 3
-
+df[['cholesterol','gluc']]=df[['cholesterol','gluc']].applymap(lambda x:0 if x==1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=["cardio"], 
+                     value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"], 
+                     var_name="variable", value_name="value")
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).size().reset_index(name="count")
     
 
     # 7
+    catplot = sns.catplot(x="variable", y="count", hue="value", col="cardio",
+                      data=df_cat, kind="bar", palette="Set2")
 
+    catplot.set_axis_labels("variable", "total")
+    catplot.set_titles("Cardio = {col_name}")
 
+    
+    return catplot.figure
 
     # 8
-    fig = None
+    fig = draw_cat_plot()
 
 
     # 9
@@ -38,20 +49,26 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
+    df_heat = df[
+        (df["ap_lo"] <= df["ap_hi"]) &  
+        (df["height"] >= df["height"].quantile(0.025)) &  
+        (df["height"] <= df["height"].quantile(0.975)) &  
+        (df["weight"] >= df["weight"].quantile(0.025)) & 
+        (df["weight"] <= df["weight"].quantile(0.975))   
+    ]
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # 15
+    sns.heatmap(corr, annot=True, fmt=".1f", mask=mask, cmap="coolwarm", center=0, linewidths=0.5, ax=ax)
 
 
 
